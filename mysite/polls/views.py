ad9d81a316d49d3e5943ba125d2d5bd500f3156a
pjm605 <pjm605@gmail.com>
2017-07-09 10:37:06 -0400

    add basic views for detail and index

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -5,12 +5,32 @@ from django.shortcuts import render
 
 # Create your views here.
 from django.http import HttpResponse
+from django.http import Http404
+from django.template import loader
+from .models import Question
+
+def index_depr(request):
+	latest_question_list = Question.objects.order_by('-pub_date')
+	template = loader.get_template('polls/index.html')
+	context = {
+		'latest_question_list': latest_question_list,
+	}
+	return HttpResponse(template.render(context, request))
 
 def index(request):
-	return HttpResponse("Hello, You are the polls index")
+	latest_question_list = Question.objects.order_by('-pub_date')
+	context = {
+		'latest_question_list': latest_question_list,
+	}
+
+	return render(request, 'polls/index.html', context)
 
 def detail(request, question_id):
-	return HttpResponse("You're looking at question %s." % question_id)
+	try:
+		question = Question.objects.get(pk=question_id)
+	except:
+		raise Http404("Question does not exist")
+	return render(request, 'polls/detail.html', {'question':question})
 
 def results(request, question_id):
 	response = "You're looking at the results of question %s."

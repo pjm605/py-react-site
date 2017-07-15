f4578b045f9aa4a98b3a59ce299d20d0fa804358
pjm605 <pjm605@gmail.com>
2017-07-15 16:02:34 -0400

    add views abstracted to classes and use django base views

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -9,6 +9,23 @@ from django.http import Http404
 from django.urls import reverse
 from django.template import loader
 from .models import Choice, Question
+from django.views import generic
+
+class IndexView(generic.ListView):
+	template_name = 'polls/index.html'
+	context_object_name = 'latest_question_list'
+	
+	def get_queryset(self):
+		return Question.objects.order_by('-pub_date')[:5]
+
+class DetailView(generic.DetailView):
+	model = Question
+	template_name = 'polls/detail.html'
+
+class ResultsView(generic.DetailView):
+	model = Question
+	template_name = 'polls/results.html'
+	
 
 def index_depr(request):
 	latest_question_list = Question.objects.order_by('-pub_date')
